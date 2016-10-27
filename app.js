62780c206731df4a37fb53c6bb188fccd3a48209
Ashley Ross <ashley.e.ross@gmail.com>
2016-10-27 08:54:04 -0700

    cleaning up js code

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 // located here as a global value that can be used for all locations
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 //functions: cookiesPerHour, predictedCustomers, renderHTMLSales
-function Location(storeName, minCust , maxCust, avgCookieSales){
-  this.storeName = storeName;
+function Location(Location, minCust , maxCust, avgCookieSales){
+  this.storeName = Location;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookieSales = avgCookieSales;
@@ -13,40 +14,40 @@ function Location(storeName, minCust , maxCust, avgCookieSales){
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   };
   //build table within this function
-
+  // table.appendChild(thead);
     // console.log('tr: ', tr);
   // creates the table header with the hours row at top
-  var th = document.createElement('th');
-  var table = document.getElementById('buildTable');
-  th.textContent = hours;
-  table.appendChild(th);
-  // create table body to house the tr that the store names live in
-  var tBody = document.createElement('tbody');
-  var tableTwo = document.getElementByID('buildTable');
-  tbody.textContent = this.XXXX;
-  tableTwo.appendChild(tBody);
 
-  var tr = document.createElement('tr');
-  var th = document.getElementByTag('th');
-  tr.textContent = ' ' + storeName;
-  th.appendChild(tr);
+  // create table body to house the tr that the store names live in
+
   // create the td cells that hold the daily cookies sales results
-  var td = document.createElement('td');
-  var tr = document.getElementByTag('tr');
-  td.textContent = this.dailyCookieSalesPerHour;
-  tr.appendChild(td);
 
   this.forEachHour = function forEachHour (){
+    var thead = document.createElement('thead');
+    var table = document.getElementByID('table');
+    thead.textContent = 'test';
+    table.appendChild(thead);
+    var tr = document.createElement('tr');
+    var thead = document.getElementById('thead');
+    tr.textContent = hours;
+    thead.appendChild(tr);
     for (var i = 0; i < hours.length; i++) {
-      var tr = document.createElement('tr');
-      th.appendChild(tr);
-
-      console.log(hours[i] + this.dailyCookieSalesPerHour[i]);
-      var td = document.createElement('td');
+      var th = document.createElement('th');
       var tr = document.getElementByTag('tr');
-      td.textContent = hours[i] + this.dailyCookieSalesPerHour[i];
+      th.textContent = ' ' + this.storeName;
+      tr.appendChild(th);
+      var td = document.createElement('td');
+      var th = document.getElementByTag('th');
+      td.textContent = this.dailyCookieSalesPerHour;
+      th.appendChild(td);
+      // th.appendChild(tr);
+      // console.log(hours[i] + this.dailyCookieSalesPerHour[i]);
+      // var td = document.createElement('td');
+      // var tr = document.getElementByTag('tr');
+      // td.textContent = this.dailyCookieSalesPerHour[i];
+      // var thead = document.createElement('thead');
       // console.log('td after textContent: ', td);
-      tr.appendChild(td);
+      // tr.appendChild(td);
       // var tr = document.createElement('tr');
       // var table = document.getElementById('buildTable');
       // th.textContent = this.storeName;
@@ -58,7 +59,7 @@ function Location(storeName, minCust , maxCust, avgCookieSales){
  // li.innerText = 'Total Sales: ' + totalSales + ' cookies';
  // ul.appendChild(li);
   };
-//generate randomCookiesSalesPerHour
+//generate randomCookiesSalesPerHour, needs to be entered in td
   this.randomCookieSalesPerHour = function (){
     for (var i = 0; i < hours.length; i++){
       this.dailyCookieSalesPerHour.push(Math.floor(this.randomCustPerHour() * this.avgCookieSales));
@@ -75,7 +76,7 @@ var allLocations = [
   new Location('Alki', 2, 16, 4.6),
 
 ];
-
+// this is what runs the data from the location array and enters it into the tr
 function forEachLocation() {
   for (var i = 0; i < allLocations.length; i++){
     console.log(allLocations[i]);
@@ -94,14 +95,14 @@ forEachLocation();
 //   location2
 // ];
 
-console.log('allLocations: ', allLocations);
+// console.log('allLocations: ', allLocations);
 
-for (var i = 0; i < hours.length; i++){
-  console.log('this: ');
-  console.log('hours[i]: ', hours[i]);
-  console.log('this.dailyCookieSalesPerHour: ', this.dailyCookieSalesPerHour);
-  console.log('this.dailyCookieSalesPerHour[i]: ', this.dailyCookieSalesPerHour[i]);
+// for (var i = 0; i < hours.length; i++){
+//   console.log('this: ');
+//   console.log('hours[i]: ', hours[i]);
+//   console.log('this.dailyCookieSalesPerHour: ', this.dailyCookieSalesPerHour);
+//   console.log('this.dailyCookieSalesPerHour[i]: ', this.dailyCookieSalesPerHour[i]);
   // (hours[i] + this.dailyCookieSalesPerHour[i]);
   // hours[i].cookiesPerHour();
   // hours[i].randomCookieSalesPerHour();
-}
+// }
