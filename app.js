d4fe474d6b0537e022d4ca685f1f7b64306d679d
Ashley Ross <ashley.e.ross@gmail.com>
2016-11-06 20:34:50 -0800

    corrected linter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var hours = ['6am' ,'6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var Pike = {
   minCust: 23,
@@ -13,18 +13,18 @@ var Pike = {
   },
 
   randomCookieSalesPerHour: function() {
-      var start = document.getElementById('pike');
-      var result = document.createElement('ul');
-      start.appendChild(result);
-  for (var i = 0; i < hours.length; i++){
-    var result2 = document.createElement('li');
-    this.cookiesPerHour.push(Math.floor(this.predictedCustomers() * this.avgCookieSales));
-    result2.innerText = hours[i] + ': ' + this.cookiesPerHour;
-    result.appendChild(result2);
-    console.log(this.cookiesPerHour);
-    // console.log(this.hours[i] + ': ' + this.cookiesPerHour[i] + ' cookies');
+    var start = document.getElementById('pike');
+    var result = document.createElement('ul');
+    start.appendChild(result);
+    for (var i = 0; i < hours.length; i++){
+      this.cookiesPerHour.push(Math.floor(this.predictedCustomers() * this.avgCookieSales));
+      var result2 = document.createElement('li');
+      result2.innerText = hours[i] + ': ' + this.cookiesPerHour;
+      result.appendChild(result2);
+      console.log(hours[i] , this.cookiesPerHour);
+      // console.log(this.hours[i] + ': ' + this.cookiesPerHour[i] + ' cookies');
+    }
   }
-}
-}
+};
 
 Pike.randomCookieSalesPerHour();
